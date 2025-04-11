142bdcd02203b6b4126145587e17aaeff43561ac
Casey Thatsanaphonh <casey.thatsanaphonh@gmail.com>
2025-04-11 15:46:49 -0700

    Changing direction of project to something more personal

---
+++ src/ProjectCard.tsx
@@ -0,0 +1,12 @@
+ const GameCard = () => {
+    return <view className="card" style={{
+        width: "150px", 
+    }}>
+        <image src="https://guildwars2.staticwars.com/wp-content/themes/guildwars2.com-live/img/og-img.f6214476.jpg" className="image"
+        />
+
+        <text className="game-title">Guild Wars 2</text>
+    </view>
+ };
+
+ export default GameCard;
